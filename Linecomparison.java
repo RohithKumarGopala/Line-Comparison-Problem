f87dee046f5e31887ed6cb3ee1413b8b12646534
“rohithgopala44” <rohithgopala44@gmail.com>
2023-02-06 21:49:29 +0530

    Added: To Check Equality Of Two Lines Is Lessthan Or Greaterthan

--- Linecomparison.java
+++ Linecomparison.java
@@ -5,7 +5,6 @@ public class Linecomparison {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter the Co-ordinates of FIRST line1 ");
         System.out.println("Enter the Co-ordinates of x1 and y1 : ");
         int x1 = sc.nextInt();
         int y1 = sc.nextInt();
@@ -13,8 +12,6 @@ public class Linecomparison {
         int x2 = sc.nextInt();
         int y2 = sc.nextInt();
         Double len1 = Math.sqrt((x2 - x1) * (x2 - x1)  +  (y2 - y1) * (y2 - y1));
-        System.out.println();
-        System.out.println("Enter the Co-ordinates of SECOND line2 ");
         System.out.println("Enter the Co-ordinates of x3 and y3 : ");
         int x3 = sc.nextInt();
         int y3 = sc.nextInt();
@@ -22,18 +19,14 @@ public class Linecomparison {
         int x4 = sc.nextInt();
         int y4 = sc.nextInt();
         Double len2 = Math.sqrt((x4 - x3) * (x4 - x3)  +  (y4 - y3) * (y4 - y3));
-        System.out.println("Length of the first Line1 is : " + len1 + " units" );
-        System.out.println("Length of the second Line2 is : " + len2 + " units" );
+        System.out.println("Length of the first Line is : " + len1 + " units" );
+        System.out.println("Length of the second Line is : " + len2 + " units" );
         System.out.println();
-        if(len1.compareTo(len2) == 1)
-            System.out.println("Length of Line 1 is GREATER than Line 2 ");
-        else if (len1.compareTo(len2) == -1)
-            System.out.println("Length of Line 1 is LESS than Line 2 ");
+        if(len1.equals(len2))
+            System.out.println("Both Lines are EQUAL ");
         else
-            System.out.println("Both lines are EQUAL in length");
+            System.out.println("NOT EQUAL lines");
 
 
     }
 }
-
-
